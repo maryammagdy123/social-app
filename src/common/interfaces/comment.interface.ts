294421036b2cc1bd791b276d953f67845f7507d1
Maryam Magdy <144924477+maryammagdy123@users.noreply.github.com>
2026-04-27 20:08:51 +0300

    feat(comment): update postId type to allow multiple post references

--- src/common/interfaces/comment.interface.ts
+++ src/common/interfaces/comment.interface.ts
@@ -1,8 +1,9 @@
 import { Types } from "mongoose";
+import { IPost } from "./post.interface";
 
 export interface IComment {
   userId: Types.ObjectId;
-  postId: Types.ObjectId;
+  postId: Types.ObjectId|IPost[];
   parentId?: Types.ObjectId | undefined; //unary relationship .. a comment may has many replies and reply it sel is comment
   content?: string | undefined;
   attachment?: string;
